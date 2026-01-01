73a193a39d022e53dc1a7ca1c48c8c61998d4e70
Margiorno <m.zawiski03@gmail.com>
2026-01-01 17:48:22 +0100

    production config

--- src/app/core/graphql/apollo.config.ts
+++ src/app/core/graphql/apollo.config.ts
@@ -9,8 +9,8 @@ import { AuthService } from '../auth/services/authService/auth.service';
 import { ErrorLinkFactory } from './apollo-ErrorLinkFactory';
 import { LoadingLinkFactory } from './apollo-LoadingLinkFactory';
 
-const prevURL = 'http://localhost:12000/graphql';
-const BASE_URL = 'http://localhost:21371';
+const prevURL = 'http://13.62.195.40:80/auth/graphql';
+const BASE_URL = 'http://13.62.195.40:80';
 
 const createClientConfig = (uri: string) => {
   const httpLink = inject(HttpLink);
